b74812e6e2647a55d32d82b0457be9308ae0e67d
Vinicius Diniz <viniciuscmbrr@gmail.com>
2024-04-18 16:44:45 -0300

    2 DIA

--- itc.py
+++ itc.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def checkTransicao(vetorVar,numEstados):
     transicao = input('digite a transicao: ').split()
     transicao0 = int(transicao[0])
@@ -11,6 +13,13 @@ def checkTransicao(vetorVar,numEstados):
     else:
         print('erro')
 
+def dicionarioTransicao(transicoes):
+    dicionarioTransicoes = {}
+    for transicao in transicoes:
+        estado_atual, simbolo, proximo_estado = transicao
+        dicionarioTransicoes[(estado_atual, simbolo)] = proximo_estado
+    return dicionarioTransicoes
+
 def checkVariaveis(vetorVar,maxSimbolos):
     variaveis =  list(input())
     if(len(variaveis) < maxSimbolos):
@@ -18,23 +27,65 @@ def checkVariaveis(vetorVar,maxSimbolos):
             if(variaveis[i] not in vetorVar):
                 print("erro")
                 ##tratar o else
-        return variaveis
+        cadeiasEntradas.append(variaveis)
     else:
         print("Passou do tamnho")
 
-##entradas
+def automato(estados, simbolos_terminais, estados_finais, estado_inicial, transicoes):
+    automato = {
+        'estados': set(estados),
+        'simbolos_terminais': set(simbolos_terminais),
+        'estados_finais': set(estados_finais),
+        'estado_inicial': estado_inicial,
+        'transicoes': dict(transicoes)
+    }
+    return automato
+
+def testeEntradas(automato, entrada):
+    fila = deque([(automato['estado_inicial'], 0)])  # Inicializa a fila com o estado inicial
+    visitados = set()  # Conjunto para rastrear os estados já visitados
+
+    while fila:
+        estado, indice = fila.popleft()  # Remove o próximo estado da fila
+        if (estado, indice) in visitados:
+            continue  # Se já visitamos este estado nesta posição, vamos para o próximo
+        visitados.add((estado, indice))  # Marca o estado atual como visitado
+
+        if estado in automato['estados_finais']:
+            if indice == len(entrada):
+                return True  # Aceita se estamos em um estado final e chegamos ao final da entrada
+
+        if indice == len(entrada):  # Se chegamos ao final da entrada
+            continue  # Vamos para o próximo estado
+
+        simbolo = entrada[indice]  # Próximo símbolo da entrada
+        transicoes = automato['transicoes'].get((estado, simbolo), set())
+        for prox_estado in transicoes:
+            fila.append((prox_estado, indice + 1))  # Adiciona os próximos estados à fila
+
+        # Se houver transições vazias
+        transicoes_vazias = automato['transicoes'].get(('', simbolo), set())
+        for prox_estado in transicoes_vazias:
+            fila.append((prox_estado, indice))  # Continua no mesmo índice
+        print(visitados)
+    return False
+        
+#Entrada do número de estados
 numEstados =  int(input("Digite os estados: ")) 
 if(numEstados >= 1  and numEstados <= 10): #
-    print()
+    estados = []
+    for i in range(numEstados):
+        estados.append(i)
 else:
     print()
 
-##var's
+#Estado inicial sempre é o 0
+estadoInicial = '0'
+
+#Entrada dos Simbolos Terminais
 readSimbolos = input("Digite o numero de simbolos:  ").split()
 numSimbolos =  int(readSimbolos[0])
-
 simbolosTerminais = []
-numEstadosFinais = input("Digite o numero de estados finais: ").split()
 
 if(numSimbolos >= 1 and numSimbolos <= 10):
     print()
@@ -44,16 +95,19 @@ else:
 for i in  range(numSimbolos):
     simbolosTerminais.append(readSimbolos[i+1])
 
+#Entrada dos Estados Finais
+readEstadosFinais = input("Digite o numero de estados finais: ").split()
+numEstadosFinais =  int((readEstadosFinais[0]))
+estadosFinais = []
 
-
-if(len(numEstadosFinais)-1 ==  int(numEstadosFinais[0])):
-    
-    for i in range(int(numEstadosFinais[0])):
-        if(int(numEstadosFinais[i+1]) < 0 or int(numEstadosFinais[i+1]) > numEstados -1):
-            print('ERRO') ## mudar no final do código
+if(len(readEstadosFinais) - 1 ==  numEstadosFinais):
+    for i in range(numEstadosFinais):
+        if(int(readEstadosFinais[i+1]) >= 0 and int(readEstadosFinais[i+1]) <= numEstados -1):
+            estadosFinais.append(readEstadosFinais[i+1])
 else:
     print(':(')
 
+simbolosTerminais.append('-')
 vetorTransicao = []
 numInteracoes = int(input('Digite o numero de interações: '))
 if(numInteracoes >= 1 and numInteracoes <= 50):
@@ -62,10 +116,10 @@ if(numInteracoes >= 1 and numInteracoes <= 50):
 else:
     print('Interacoes erro')
 
+transicoes = dicionarioTransicao(vetorTransicao)
 
-
-
-
+cadeiasEntradas= []
+simbolosTerminais.remove('-')
 numEntradas = int(input('Digite o numero de entradas: '))
 if(numEntradas >= 1 and numEntradas <= 10):
      for i in range(numEntradas):  
@@ -73,4 +127,10 @@ if(numEntradas >= 1 and numEntradas <= 10):
 else:
     print('Entradas erro')
 
+autômato = automato(estados, simbolosTerminais, estadosFinais, estadoInicial, transicoes)
 
+for i in range(len(cadeiasEntradas)):
+    if testeEntradas(autômato, cadeiasEntradas[i]) == True:
+        print('aceita')
+    else:
+        print('rejeita')
